ad1e2e5f90a85e97a019f309d0f5d52cc6f75d9b
yxb123456cy <2186256471@qq.com>
2026-01-04 11:37:50 +0800

    feat(config): :sparkles: 引入antfu ESlint-Config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,6 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+export default antfu({
+  react: true,
+})
